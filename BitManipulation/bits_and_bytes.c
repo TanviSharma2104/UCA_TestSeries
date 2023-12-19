2c5dae3587f501d2ba59a4ec2727c4db8293dabb
Tanvi_Sharma <101709149+TanviSharma2104@users.noreply.github.com>
2023-12-19 22:41:33 +0530

    Update bits_and_bytes.c  with solutions

--- BitManipulation/bits_and_bytes.c
+++ BitManipulation/bits_and_bytes.c
@@ -1,47 +1,45 @@
-/*
 /*  bitAnd - x & y using only ~ and |
 /* Example bitAnd(6, 5) = 4
 */ Legal ops:  ~ |  
 /*Max ops: 8
-*/
+
 int bitAnd (int x, int y) {
-     return 0;
+     return ~((~x) | (~y));
 }
 
-/*
+
+
 /*  bitXor - x ^ y using only ~ and &
 /* Example bitXor(4, 5) = 1
 */ Legal ops:  ~ &  
 /* Max ops: 14
-*/
+
 int bitXor (int x, int y) {
-     return 0;
+     return ((~x) & y) | (x & (~y));
 }
 
 
 
-  /*
 * sign - return 1 if positive, 0 if zero, and -1 if negative
 * Examples: sign(130) = 1, sign(-23) = -1
 * Legal ops: ! ~ & ^ | + << >>
 * Max ops : 10
-*/
+
 int sign(int x) {
-   //TODO
-    return 0;
+    return (x>>31) | (!!x);
 }
 
 
 
-  /*
 * getByte - extract byte n from word x
 * Examples: getByte(0x12345678, 1)  = 0x56
 *Legal ops: ! ~ & ^| + << >>
 Max ops: 6
-*/
+
 int getByte(int x, int n) {
-    return 0;
+    return (x >> (n<<3)) & 0xFF;
 }
+
 
 
   /* logicalShift - shift x to the right by n, using a logical shift
@@ -49,22 +47,24 @@ int getByte(int x, int n) {
 * examples: logicalShift(0x87654321, 4) = 0x8765432
 *  Legal ops:  ~ & ^ | + << >>
 *Max ops: 20
-*/
+
 int logicalShift(int x, int n) {
-    return 0;
+    int mask = ~(((1 << 31) >> n) << 1);
+    return (x >> n) & mask;
 }
 
 
 
-  /*
 * conditional - same as x ? y : z
 * example conditional (2, 4, 5) = 4
 * ! ~ & ^ | + << >>
 * Max ops: 16
-*/
+
 int conditional(int x, int y, int z) {
-    return 0;
+     int mask = ((!x) << 31) >> 31;
+     return (y & mask) | (z & (~mask));
 }
+
 
 
   /* bang - Compute !x without using !
@@ -73,18 +73,17 @@ int conditional(int x, int y, int z) {
 *  Max ops: 12
 */ 
 int bang(int x) {
-    return 0;
+    return (((x | (~x + 1)) >> 31) & 1) ^ 1;
   }
 
 
-
-  /* 
+ 
 /* Return x with the n bits that begin at position p inverted (i.e. turn 0 /* into 1 and vice versa)
 /* and the rest left unchanged. Consider the indices of x to begin with the /* lower -order bit   numbered
 /* Legal ops: ~ & ^ | << >>
 /* as zero
-*/
+
 int invert (int x, int p, int n) {
-    //TODO
-    return 0;
+    int mask = ~(((1 << n) - 1) << p);
+    return x ^ mask;
 }
